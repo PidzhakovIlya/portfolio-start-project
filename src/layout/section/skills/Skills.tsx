18bfa36fad5b4b3c4e6329c474979d39e5d7660a
PidzhakovIlya <pidzhakovilja@gmail.com>
2023-07-29 14:29:53 +0300

    Styled section skill and works

--- src/layout/section/skills/Skills.tsx
+++ src/layout/section/skills/Skills.tsx
@@ -1,59 +1,67 @@
 import React from "react";
-import { styled } from "styled-components";
-import { FlexWrapper } from "../../../components/FlexWrapper";
-import { SectionTitle } from "../../../components/SectionTitle";
-import { StyledSkill } from "./skill/StyledSkill";
+import {styled} from "styled-components";
+import {FlexWrapper} from "../../../components/FlexWrapper";
+import {SectionTitle} from "../../../components/SectionTitle";
+import {StyledSkill} from "./skill/StyledSkill";
+import {Container} from "../../../components/Container";
 
 export const MySkils = () => {
     return (
         <StyledSectionSkills>
-            <SectionTitle>Projects</SectionTitle>
-            <FlexWrapper direction="column" align="center">
-                <StyledSkill nameSkil="Html" experiens="Beginner" />
-                <StyledSkill nameSkil="CSS, Sass & Bootstrap" experiens="Beginner" />
-                <StyledSkill nameSkil="JavaScript, TypeScript, JQuery" experiens="Beginner" />
-                <StyledSkill nameSkil="UI design in Figma" experiens="Beginner" />
-                <StyledSkill nameSkil="Angular" experiens="Beginner" />
-                <StyledSkill nameSkil="React" experiens="Beginner" />
-                <StyledSkill nameSkil="React Native" experiens="Beginner" />
-            </FlexWrapper>
-            <SubTitle>Additional technologies and skills</SubTitle>
-            <FlexWrapper>
-                <SkillList>
-                    <li>Git</li>
-                    <li>Wordpress</li>
-                    <li>Teamwork</li>
-                    <li>Quick learning</li>
-                    <li>Engagement</li>
-                    <li>B2 english</li>
-                    <li>RWD</li>
-                </SkillList>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>Projects</SectionTitle>
+                <FlexWrapper direction="column" align="center" gap={"55px"}>
+                    <StyledSkill nameSkil="Html" experiens="Beginner"/>
+                    <StyledSkill nameSkil="CSS, Sass & Bootstrap" experiens="Beginner"/>
+                    <StyledSkill nameSkil="JavaScript, TypeScript, JQuery" experiens="Beginner"/>
+                    <StyledSkill nameSkil="UI design in Figma" experiens="Beginner"/>
+                    <StyledSkill nameSkil="Angular" experiens="Beginner"/>
+                    <StyledSkill nameSkil="React" experiens="Beginner"/>
+                    <StyledSkill nameSkil="React Native" experiens="Beginner"/>
+                </FlexWrapper>
+                <SubTitle>Additional technologies and skills</SubTitle>
+                    <SkillList>
+                        <li>Git</li>
+                        <li>Wordpress</li>
+                        <li>Teamwork</li>
+                        <li>Quick learning</li>
+                        <li>Engagement</li>
+                        <li>B2 english</li>
+                        <li>RWD</li>
+                    </SkillList>
+            </Container>
         </StyledSectionSkills>
     )
 }
 
 const StyledSectionSkills = styled.section`
-    min-height: 100vh;
-    
+  min-height: 100vh;
+
 `
 const SkillList = styled.ul`
   max-width: 1920px;
   width: 100%;
-  display: flex;
-  justify-content: space-evenly;
-  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-auto-rows: minmax(5px, auto);
+  justify-content: center;
+  align-content: center;
+  justify-items: center;
+  align-items: center;
 
   li {
     margin: 30px;
     align-self: center;
     width: 25%;
+    font-size: 36px;
   }
 
 
 `
 
 const SubTitle = styled.h3`
-    font-size: 48px;
-    text-align: center;
+  font-size: 48px;
+  text-align: center;
+  margin-top: 160px;
+  margin-bottom: 80px;
 `
--- src/layout/section/works/work/Work.tsx
+++ src/layout/section/works/work/Work.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
+import {FlexWrapper} from "../../../../components/FlexWrapper";
 
 
 type WorkPropsType = {
@@ -10,24 +11,25 @@ type WorkPropsType = {
 export const Work = (props: WorkPropsType) => {
     return (
         <StyledWork>
-            <Image src={props.src} alt="project-1"/>
-            <Title>{props.title}</Title>
-            <Description>{props.text}</Description>
-            <button>Look it up</button>
+            <FlexWrapper direction={"column"} justify={"space-between"} >
+                <Image src={props.src} alt="project-1"/>
+                <Title>{props.title}</Title>
+                <Description>{props.text}</Description>
+                <button>Look it up</button>
+            </FlexWrapper>
         </StyledWork>
     );
 };
-
 
 
 const StyledWork = styled.div`
   max-width: 550px;
   width: 100%;
   height: 700px;
-  border-radius: 0 50px;
+  border-radius: 50px 0;
   border: 1px solid #A39D9D;
   background: #0F1624;
-  box-sizing: border-box;
+  padding: 25px;
 
 
 `
@@ -35,13 +37,32 @@ const Image = styled.img`
   width: 100%;
   height: 266px;
   object-fit: cover;
+  border-radius: 24px 8px 8px 8px;
 `
 
 
 const Description = styled.p`
-
+  text-align: start;
+  font-size: 28px;
 `
 const Title = styled.h3`
   color: #FFF;
   font-size: 36px;
+  position: relative;
+  text-align: start;
+  
+  
+  &::before {
+    content: "";
+    display: inline-block;
+    position: absolute;
+    bottom: -25px;
+    left: 50%;
+    transform: translateX(-50%);
+    max-width: 300px;
+    width: 100%;
+    height: 4px;
+    background-image: linear-gradient(270deg, #13ADC7 0%, #6978D1 66.67%, #945DD6 100%);
+    border-radius: 83px;
+  }
 `
